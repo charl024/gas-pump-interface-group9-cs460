ea05664942d2ebd9086e18d2f02b1b1ba7518699
Kyle V Nguyen <vikhang04@unm.edu>
2025-08-29 12:47:51 -0600

    start flow meter

--- src/FlowMeter/FMDisplay.java
+++ src/FlowMeter/FMDisplay.java
@@ -1,13 +1,43 @@
 package FlowMeter;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 //Flow meter tells how much gas has been pumped
 //When gas is finished pumping, and a new car starts pumping, reset volume
 // count to zero
 public class FMDisplay {
-    private Pane pane;
+    private final BorderPane pane; //Where all images/text will be placed on
+    private final Button stopFuel;
+    private final Label costInfo;
+    private final Label volInfo;
+    private HBox info;
+    private double cost = 0;
+    private double vol = 0;
+    private double gasRate;
+    private double volRate;
+    //Would an actual progress bar be helpful? If so, what is considered
+    // being full/how would we know
+
 
     public FMDisplay(){
-        
+        pane = new BorderPane();
+        stopFuel = new Button("Stop");
+        costInfo = new Label("Cost: $" + cost);
+        volInfo = new Label(vol + " Gallons");
+        info = new HBox(costInfo, volInfo);
+
+        pane.setCenter(info);
+        pane.setRight(stopFuel);
+
+
     }
+
+    //create methods that will update current cost and volumes
+//    public static void main(String[] args) {
+//        FMDisplay fmDisplay = new FMDisplay();
+//
+//    }
 }
--- src/FlowMeter/FlowMeter.java
+++ src/FlowMeter/FlowMeter.java
@@ -1,4 +1,9 @@
 package FlowMeter;
 
 public class FlowMeter {
+    //When connection is made with IO, save port information and sockets in
+    // order to send information back if needed
+    public FlowMeter() {
+
+    }
 }
