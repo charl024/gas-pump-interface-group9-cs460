84dba4ee21930100cd7f4f7c555d38f9c483b3e6
charl024 <comaoeng2003@gmail.com>
2025-08-29 10:46:45 -0600

    Added functionality to IOPort and some tests for IOPort in Main. read() is bugged, it hangs after get() request.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import IOPort.IOPort;
+import Screen.Screen;
 
 class Main {
     public static void main(String[] args) {
@@ -6,6 +8,13 @@ class Main {
           create and connect stuff here maybe (for testing)?
          */
 
+
+        IOPort screenPort = new IOPort(5050);
+
+        screenPort.send("test!");
+
+        System.out.println(screenPort.read());
+        System.out.println(screenPort.get());
 
         System.out.println("Hello world!");
     }
