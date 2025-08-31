41b7a33100b225537a6d0a57d44dd46058efdba9
Kyle V Nguyen <vikhang04@unm.edu>
2025-08-30 20:59:45 -0600

    Message class

--- src/MessagePassed/Message.java
+++ src/MessagePassed/Message.java
@@ -1,4 +1,13 @@
 package MessagePassed;
 
 public class Message {
+    private String description; //No clue what to name this, don't want same
+    // name as the class
+    public Message(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
