2386c0881ffbf3b8199260dae89d51c6b9752cfe
Kyle V Nguyen <vikhang04@unm.edu>
2025-09-18 00:07:05 -0600

    Update card reader so that it can be ran without being connected to IOPort

--- src/CardReader/CRMain.java
+++ src/CardReader/CRMain.java
@@ -14,16 +14,70 @@ import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
+import java.util.List;
+
 /**
  * Main for Card Reader
  */
 public class CRMain extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        CommPort port = new CommPort(3);
-        CardReader cardReader = new CardReader(port);
+        System.out.println("Error");
+        List<String> arguments = getParameters().getRaw();
+        //If we receive an argument then we should start the program without
+        // connecting to anything
+        if (arguments.size() == 1) {
+            CardReader cardReader = new CardReader();
+
+            createPane(primaryStage, cardReader);
+
+            primaryStage.setOnCloseRequest(event -> {
+                Platform.exit();
+            });
+
+            primaryStage.show();
+        } else if (arguments.isEmpty()) {
+
+            CommPort port = new CommPort(3);
+            CardReader cardReader = new CardReader(port);
 
 
+            createPane(primaryStage, cardReader);
+
+            primaryStage.setOnCloseRequest(event -> {
+                port.close();
+                Platform.exit();
+            });
+
+            primaryStage.show();
+
+            new Thread(() -> {
+                try {
+                    while (true) {
+                        Thread.sleep(10); // wait 10ms
+                        Message message = port.get();         // blocking call
+                        if (message != null) {
+                            cardReader.getClient().handleMessage(message);
+                        }
+                    }
+
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }).start();
+        } else {
+            System.out.println("Invalid number of arguments");
+        }
+    }
+
+    /**
+     * Creates the pane that will contain everything
+     *
+     * @param primaryStage Stage
+     * @param cardReader   Card Reader
+     */
+    private void createPane(Stage primaryStage, CardReader cardReader) {
         StackPane pane = new StackPane();
         pane.setMinSize(400, 400);
         pane.setBackground(new Background(new BackgroundFill(Color.GREY,
@@ -31,30 +85,7 @@ public class CRMain extends Application {
         pane.getChildren().add(cardReader.getDisplay().getPane());
         pane.setAlignment(Pos.CENTER);
         primaryStage.setScene(new Scene(pane));
-
-        primaryStage.setOnCloseRequest(event -> {
-            port.close();
-            Platform.exit();
-        });
-
-        primaryStage.show();
-
-        new Thread(() -> {
-            try {
-                while (true) {
-                    Thread.sleep(10); // wait 10ms
-                    Message message = port.get();         // blocking call
-                    if (message != null) {
-                        cardReader.getClient().handleMessage(message);
-                    }
-                }
-
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }).start();
-
+        System.out.println("got here");
     }
 
     /**
