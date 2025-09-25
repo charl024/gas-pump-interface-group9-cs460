224d2acf6fdde5cc8d932884d95a2956f4da10e8
charl024 <comaoeng2003@gmail.com>
2025-09-24 22:57:45 -0600

    Created GasStationManager, our top level control program.

---
+++ src/GasStationManager.java
@@ -0,0 +1,15 @@
+import PumpAssemblyManager.PumpAssemblyManager;
+
+public class GasStationManager {
+
+    public GasStationManager() {
+        PumpAssemblyManager pumpAssembly = new PumpAssemblyManager();
+
+
+    }
+
+    public static void main(String[] args) {
+        new GasStationManager();
+    }
+
+}
