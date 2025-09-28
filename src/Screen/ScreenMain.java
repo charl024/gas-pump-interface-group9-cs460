a890811abdb42ce53ae1707ef7b88d10a32f447a
pandaweasel <ddavis11@unm.edu>
2025-09-28 12:53:21 -0600

    deleted instance of the server in screen main (its being called in handle message)

--- src/Screen/ScreenMain.java
+++ src/Screen/ScreenMain.java
@@ -24,7 +24,6 @@ public class ScreenMain extends Application {
         screenDisplay.showPumpUnavailableScreen();
 
         handleMessage = new HandleMessage(screenDisplay);
-        ScreenServer  screenServer = new ScreenServer(PortLookupMap.PortMap(6),handleMessage);
         //TODO have screen display have an instance of the server
         primaryStage.show();
     }
