3dc7eeb7a35ccd43e3299c4ac42bf2b8f2f8ddae
charl024 <comaoeng2003@gmail.com>
2025-09-30 10:08:33 -0600

    Fixed these two bugs: screen times out if pumping takes too long, proceeds to pumping complete after pumping is done and, total screen will show up, if we disconnect the hose, screen shows "pump paused"

--- src/Screen/HandleMessage.java
+++ src/Screen/HandleMessage.java
@@ -13,6 +13,8 @@ public class HandleMessage {
     private ScreenDisplay.PossibleActionsForButton possibleActions;
     private Timer timer;
 
+    private boolean pumping = false;
+
     // TODO //IMPORTANT// Need to change to actual port number and fix this
     private ScreenServer server;
 
@@ -22,213 +24,115 @@ public class HandleMessage {
         new Thread(server).start();
     }
 
-    /*        String message examples
-    //        This message is used to change things like
-    //        the font style, font size, button color, given button an action, give label some text
-    //        messageStr = "SC-i.4-12.4-re.9-3.6";
-    //        This message is used to add text to a desired label (0)
-    //        messageStr = "SC-Here is some example text.0";
-    //        This message initiates welcome screen
-    //        messageStr = "SC-welcome";
-    //        This message lets us know if card was accepted
-    //        messageStr = "SC-accepted";
-    //        This message lets us know if card was denied
-    //        messageStr = "SC-denied";
-    //        This message initiates gas option screen
-    //        messageStr = "SC-gas";
-    //        This message shows receipt screen
-    //        messageStr = "SC-receipt";
-     */
-
     public void handleMessage(Message msg) {
-
-
         String messageStr = msg.getDescription();
-
         String[] parts = messageStr.split("-");
 
         String deviceType = parts[0];
         if (!(deviceType.equals("SC"))) {
             Message invalidMessage = new Message("SC-INVALID");
             server.sendMessage(invalidMessage);
-        } else {
+            return;
+        }
 
-//            if (parts.length == 2) {
-            if (parts[1].equals("INITIALPRICE")) {
+        if (parts[1].equals("INITIALPRICE")) {
+            screenDisplay.setPrices(
+                    Double.parseDouble(parts[2]),
+                    Double.parseDouble(parts[3]),
+                    Double.parseDouble(parts[4]));
+            Platform.runLater(() -> {
+                screenDisplay.resetLabels();
+                screenDisplay.showWelcomeScreen();
+            });
 
-                screenDisplay.setPrices(
-                        Double.parseDouble(parts[2]),
-                        Double.parseDouble(parts[3]),
-                        Double.parseDouble(parts[4]));
-                Platform.runLater(() -> {
+        } else if (parts[1].equals("AUTHORIZING")) {
+            Platform.runLater(() -> {
+                screenDisplay.resetLabels();
+                screenDisplay.showAuthorizationScreen();
+            });
+            timeoutTimer();
+
+        } else if (parts.length >= 3 && parts[2].equals("VALIDCARD")) {
+            cancelTimeout();
+            Platform.runLater(() -> {
+                screenDisplay.resetLabels();
+                screenDisplay.showCardAcceptedScreen();
+                scheduleSingleTask(() -> {
+                    screenDisplay.resetLabels();
+                    initiateGasSelection();
+                }, 5000);
+            });
+
+        } else if (parts.length >= 3 && parts[2].equals("INVALIDCARD")) {
+            cancelTimeout();
+            Platform.runLater(() -> {
+                screenDisplay.resetLabels();
+                screenDisplay.showCardDeniedScreen();
+                scheduleSingleTask(() -> {
                     screenDisplay.resetLabels();
                     screenDisplay.showWelcomeScreen();
-                });
-
-            } else if (parts[1].equals("AUTHORIZING")) {
-                Platform.runLater(() -> {
-                    screenDisplay.resetLabels();
-                    screenDisplay.showAuthorizationScreen();
-                });
-
-
-                timeoutTimer();
-
-            } else if (parts.length >= 3 && parts[2].equals("VALIDCARD")) {
-                cancelTimeout();
-
-                Platform.runLater(() -> {
-                    screenDisplay.resetLabels();
-                    screenDisplay.showCardAcceptedScreen();
-                    timer = new Timer();
-
-                    timer.schedule(new TimerTask() {
-                        @Override
-                        public void run() {
-                            Platform.runLater(() -> {
-                                screenDisplay.resetLabels();
-                                initiateGasSelection();
-                            });
-
-                        }
-                    }, 5000);
-                });
-
-//                timer = new Timer();
-//
-//                timer.schedule(new TimerTask() {
-//                    @Override
-//                    public void run() {
-//                        screenDisplay.resetLabels();
-//                        initiateGasSelection();
-//                    }
-//                }, 5000);
-            } else if (parts.length >= 3 && parts[2].equals("INVALIDCARD")) {
-
-                cancelTimeout();
-
-                Platform.runLater(() -> {
-                    screenDisplay.resetLabels();
-                    screenDisplay.showCardDeniedScreen();
-                    timer = new Timer();
-                    timer.schedule(new TimerTask() {
-                        @Override
-                        public void run() {
-                            Platform.runLater(() -> {
-                                screenDisplay.resetLabels();
-                                screenDisplay.showWelcomeScreen();
-                            });
-                        }
-                    }, 10000);
-                });
-
-
-            } else if (parts[1].equals("PUMPINGPROGRESS")) {
-                cancelTimeout();
-                Platform.runLater(() -> {
-                    screenDisplay.resetLabels();
-                    screenDisplay.showPumpingProgress();
-                });
-
-            } else if (parts[1].equals("HOSEPAUSED")) {
-                Platform.runLater(() -> {
-                    screenDisplay.resetLabels();
-                    screenDisplay.showHosePausedScreen();
-                });
-
-                timer = new Timer();
-                timer.schedule(new TimerTask() {
-                    @Override
-                    public void run() {
-                        Platform.runLater(() -> {
-                            screenDisplay.resetLabels();
-                            screenDisplay.showTimeoutScreen();
-                            sendServerMessage(new Message("SC-NEWTOTAL"));
-                        });
-
-                    }
                 }, 10000);
-            } else if (parts[1].equals("NEWTOTAL")) {
-                Platform.runLater(() -> {
-                    screenDisplay.resetLabels();
+            });
 
-                    screenDisplay.showFuelFinishedScreen(
-                            Double.parseDouble(parts[2]),
-                            Double.parseDouble(parts[3]));
-                });
+        } else if (parts[1].equals("PUMPINGPROGRESS")) {
+            pumping = true;
+            cancelTimeout();
+            Platform.runLater(() -> {
+                screenDisplay.resetLabels();
+                screenDisplay.showPumpingProgress();
+            });
 
+        } else if (parts[1].equals("HOSEPAUSED")) {
+            // if not pumping, return (dont do anything)
+            if (!pumping) return;
+            Platform.runLater(() -> {
+                screenDisplay.resetLabels();
+                screenDisplay.showHosePausedScreen();
+            });
+            scheduleSingleTask(() -> {
+                if (!pumping) return;
+                screenDisplay.resetLabels();
+                screenDisplay.showTimeoutScreen();
+                sendServerMessage(new Message("SC-NEWTOTAL"));
+            }, 10000);
 
-                timer = new Timer();
-                timer.schedule(new TimerTask() {
-                    @Override
-                    public void run() {
-                        Platform.runLater(() -> {
-                            screenDisplay.resetLabels();
-                            screenDisplay.showWelcomeScreen();
-                        });
+        } else if (parts[1].equals("NEWTOTAL")) {
+            pumping = false;
+            Platform.runLater(() -> {
+                screenDisplay.resetLabels();
+                screenDisplay.showFuelFinishedScreen(
+                        Double.parseDouble(parts[2]),
+                        Double.parseDouble(parts[3]));
+            });
+            scheduleSingleTask(() -> {
+                screenDisplay.resetLabels();
+                screenDisplay.showWelcomeScreen();
+            }, 10000);
 
-                    }
-                }, 10000);
-            } else if (parts[1].equals("CHANGEPRICES")) {
+        } else if (parts[1].equals("CHANGEPRICES")) {
+            Platform.runLater(() -> {
                 screenDisplay.updateGasPrices(
                         Double.parseDouble(parts[2]),
                         Double.parseDouble(parts[3]),
                         Double.parseDouble(parts[4]));
-            }
-
-
-//                else if (parts[1].equals("PUMPUNAVAILABLE")) {
-//                    screenDisplay.resetLabels();
-//                    screenDisplay.showPumpUnavailableScreen();
-//                }
-//            }
-
-            //MAY NEED TO DELETE THIS CODE, ASK KYLE
-
-//            if (parts.length == 3) {
-//                screenDisplay.writeText(parts[1], Integer.parseInt(parts[2]));
-//                return;
-//            }
-//            if (!parts[1].equals("*")) {
-//                String[] s = parts[1].split("\\.");
-//                screenDisplay.changeFont(s[0], Integer.parseInt(s[1]));
-//            }
-//            if (!parts[2].equals("*")) {
-//                String[] s = parts[2].split("\\.");
-//                screenDisplay.changeTextSize(Integer.parseInt(s[0]), Integer.parseInt(s[1]));
-//            }
-//            if (!parts[3].equals("*")) {
-//                // may remove this block of code
-//                //Start
-//                String[] s = parts[3].split("\\.");
-//                screenDisplay.changeButtonColor(
-//                        screenDisplay.convertColor(s[0]),
-//                        Integer.parseInt(s[1])
-//                );
-//                //End
-//                screenDisplay.changeButtonColorV2(
-//                        screenDisplay.convertColorV2(s[0]),
-//                        Integer.parseInt(s[1])
-//                );
-//            }
-//            if (!parts[4].equals("*")) {
-//                String[] s = parts[4].split("\\.");
-//                screenDisplay.giveButtonAction(Integer.parseInt(s[0]), Integer.parseInt(s[1]));
-//            }
-
+            });
         }
     }
 
     public void timeoutTimer() {
+        if (pumping) return;
+        // prevent duplicate timers
+        cancelTimeout();
         timer = new Timer();
 
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
+                if (pumping) return;
                 Platform.runLater(() -> {
                     screenDisplay.resetLabels();
                     screenDisplay.showTimeoutScreen();
-                    System.out.println("Timeout triggered!"); // debug message
+                    System.out.println("Timeout triggered!");
                 });
             }
         }, 60000);
@@ -236,10 +140,11 @@ public class HandleMessage {
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
+                if (pumping) return;
                 Platform.runLater(() -> {
                     screenDisplay.resetLabels();
                     screenDisplay.showWelcomeScreen();
-                    System.out.println("Back to welcome screen"); // debug message
+                    System.out.println("Back to welcome screen");
                 });
             }
         }, 70000);
@@ -247,28 +152,37 @@ public class HandleMessage {
 
     public void cancelTimeout() {
         if (timer != null) {
-            timer.cancel(); // cancels all scheduled tasks
+            timer.cancel();
             timer = null;
         }
     }
 
-    public void transactionCancel() {
-        screenDisplay.resetLabels();
-        screenDisplay.showTransactionCanceledScreen();
-        timer = new Timer();
-        timer.schedule(new TimerTask() {
+    private void scheduleSingleTask(Runnable action, long delayMs) {
+        // run a single delayed task without interfering with timeout timers.
+        Timer t = new Timer();
+        t.schedule(new TimerTask() {
             @Override
             public void run() {
-                Platform.runLater(() -> {
-                    screenDisplay.resetLabels();
-                    screenDisplay.showWelcomeScreen();
-                });
+                if (pumping) return; // safeguard
+                Platform.runLater(action);
             }
+        }, delayMs);
+    }
+
+    public void transactionCancel() {
+        pumping = false;
+        cancelTimeout();
+        Platform.runLater(() -> {
+            screenDisplay.resetLabels();
+            screenDisplay.showTransactionCanceledScreen();
+        });
+        scheduleSingleTask(() -> {
+            screenDisplay.resetLabels();
+            screenDisplay.showWelcomeScreen();
         }, 10000);
     }
 
     private void initiateGasSelection() {
-
         screenDisplay.showGasSelectionScreen();
         screenDisplay.setOnGasSelection();
         timeoutTimer();
